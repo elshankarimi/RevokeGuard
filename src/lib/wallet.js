781c9061389ff7f1b3bd8c2c12a230722386de04
elshankarimi <130746933+elshankarimi@users.noreply.github.com>
2025-12-11 02:27:00 +0330

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -1,22 +1,18 @@
-import { configureChains, createClient } from 'wagmi'
+import { createConfig, http } from 'wagmi'
 import { mainnet, polygon, arbitrum, optimism, base } from 'wagmi/chains'
-import { publicProvider } from 'wagmi/providers/public'
 import { walletConnect, injected } from 'wagmi/connectors'
 
 const projectId = 'ac634d78fb9387e384997db507c695b3'
 
-const { chains, provider } = configureChains(
-  [mainnet, polygon, arbitrum, optimism, base],
-  [publicProvider()]
-)
+const chains = [mainnet, polygon, arbitrum, optimism, base]
 
-export const wagmiClient = createClient({
+export const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: [
     walletConnect({ projectId, chains }),
     injected({ chains })
   ],
-  provider
+  transports: Object.fromEntries(chains.map(chain => [chain.id, http()])),
 })
 
 export { chains }
