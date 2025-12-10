9732d496e64968a65e7728de0f13a069d4d98069
elshankarimi <130746933+elshankarimi@users.noreply.github.com>
2025-12-10 23:28:33 +0330

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// اگر نیاز به alias داری (مثلاً برای src یا node_modules)، اینجا اضافه کن
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': '/src', // حالا می‌تونی به جای ../../ از "@/..." استفاده کنی
-    },
+    alias: { '@': '/src' },
   },
-  server: {
-    port: 3000,
+  optimizeDeps: {
+    include: [
+      'viem',
+      'wagmi',
+      '@web3modal/wagmi',
+      '@walletconnect/universal-provider'
+    ],
   },
-  build: {
-    outDir: 'dist',
-  },
+  server: { port: 3000 },
+  build: { outDir: 'dist' },
 })
