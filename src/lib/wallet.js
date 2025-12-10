d1338b9e55c7916b455d5db2c837baf1672acfd6
elshankarimi <130746933+elshankarimi@users.noreply.github.com>
2025-12-11 03:21:16 +0330

    Update wallet.js

--- src/lib/wallet.js
+++ src/lib/wallet.js
@@ -2,7 +2,8 @@
 import { configureChains, createClient } from 'wagmi'
 import { mainnet, polygon, arbitrum, optimism, base } from 'wagmi/chains'
 import { injectedWallet, walletConnectWallet } from '@rainbow-me/rainbowkit/wallets'
-import { publicProvider } from '@wagmi/providers/public' // <-- مسیر درست در نسخه جدید wagmi
+// مسیر درست publicProvider در نسخه‌های جدید
+import { publicProvider } from 'wagmi/providers/public'
 
 const chains = [mainnet, polygon, arbitrum, optimism, base]
 
